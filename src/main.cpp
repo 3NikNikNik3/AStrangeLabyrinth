840084ecf40f7a9553cea9064f7d5863df67f463
3NikNikNik3 <none>
2025-01-31 19:47:00 +0300

    ITestedAndNewTestRoom

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,28 @@
 using namespace AStrangeLabyrinth;
 
 int main() {
-    Tiles::Tile q, w;
-    w.go[0] = &q;
-    q.go[2] = &w;
-    w.go[1] = &w;
-    w.go[3] = &w;
+    Tiles::Tile q, w, e, r;
+
+    q.go[0] = &w;
+    w.go[2] = &q;
+    w.go[1] = &e;
+    e.go[3] = &w;
+    e.go[2] = &r;
+    r.go[0] = &e;
+    r.go[3] = &q;
+    q.go[1] = &r;
+
+    Tiles::Tile a;
+    q.go[1] = &a;
+    a.go[3] = &q;
+
+    a.go[1] = &a;
+
     q.update_boards();
     w.update_boards();
+    e.update_boards();
+    r.update_boards();
+    a.update_boards();
 
     sf::RenderWindow window(sf::VideoMode({800, 600}), "A strange labyrinth", sf::Style::Default);
 
